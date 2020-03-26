b032c6eeeb75b857efaada2c00a5b8c05a81bfb0
Mack McQuen <mackmcquen@comcast.net>
2020-03-26 23:08:36 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,5 +18,19 @@ def directors_totals(nds)
   #
   #
   # Be sure to return the result at the end!
-  nil
+  pp directors_database
+  totals = {}
+  director_index = 0
+  while director_index < nds.length do
+    movie_index = 0
+    movie_gross = 0
+  while movie_index < nds[director_index][:movies].length do
+    movie_gross += nds[director_index][:movies][movie_index][:worldwide_gross]
+    movie_index += 1
 end
+    totals[nds[director_index][:name]] = movie_gross
+    director_index += 1
+end
+pp totals
+end
+  
